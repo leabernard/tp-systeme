b3511e2f00d639f8bfbd14d16ab7dda04fa34952
leariko_ <70693222+leabernard@users.noreply.github.com>
2021-10-21 08:28:57 +0200

    Delete projet dmx/dmx1/dmx1 directory

--- projet dmx/dmx1/dmx1/dmx1.cpp
+++
@@ -1,27 +0,0 @@
-#include "dmx1.h"
-
-DMX::DMX()
-{
-	g_dasusbdll = LoadLibrary(L"DasHard2006.dll");
-
-	if(g_dasusbdll)
-		DasUsbCommand = (DASHARDCOMMAND)::GetProcAddress((HMODULE)g_dasusbdll, "DasUsbCommand");	interface_open = DasUsbCommand(DHC_INIT, 0, NULL);	lblResultOpenDMX->Caption = interface_open;
-	interface_open = DasUsbCommand(DHC_OPEN, 0, 0);
-	lblResultOpenDMX->Caption = interface_open;
-}
-
-void DMX::SendTrame()
-{
-	if (interface_open > 0)
-	{
-		
-		int i;
-		for (i = 0; i < DMX_MAXCHANNEL + 1; i++) {
-			dmxBlock[i] = 0;
-	
-
-	}
-		DasUsbCommand(DHC_DMXOUT, DMX_MAXCHANNEL, dmxBlock);
-		Timer1->Enabled = true;
-}
-
--- projet dmx/dmx1/dmx1/dmx1/dmx1/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QtCore/QCoreApplication>
-#include "dmx1.h"
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-	DMX * obj = new DMX();
-	obj->SendTrame();
-    return a.exec();
-}
